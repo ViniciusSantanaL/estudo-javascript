3e8499ca3808952f75f9aa1fc012af8adb32c04d
Vinicius Santana <viniciussantanaleao@gmail.com>
2022-01-07 21:08:07 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,20 @@
+class Cliente{
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
+
+
+ cliente1 = new Cliente();
+cliente1.nome = "Ricardo";
+cliente1.cpf = 060606006;
+cliente1.agencia = 1001;
+cliente1.saldo = 0;
+
+const cliente2Nome = "Alice";
+const cliente2CPF = 234242425;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
+console.log(cliente1);
