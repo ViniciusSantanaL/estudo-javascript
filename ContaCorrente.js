82f7f76fb2ad17b7e1c57705c4bfe50beaba333b
vinicius.vsls <vssilva6@latam.stefanini.com>
2022-01-13 19:21:09 -0300

    Refatorando Cadastro de contas em memoria e regras de transções

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,20 +1,43 @@
 export class ContaCorrente{
     agencia;
     #saldo = 0;
+    cliente;
 
+    
     sacar(valor){
-        if(this.#saldo >= valor){
-            this.#saldo -= valor;
+        if(this.#saldo >= valor && valor > 0){
+            this.#saldo -= valor
+            console.log("Valor atual do Saldo após saque: R$"+ this.#saldo);
         }else{
-            console.log('Saldo indisponivel para saque');
+            console.log('Saldo indisponivel para saque, Saldo: R$' + this.#saldo);
         }
     }
     deposito(valor){
-        if(valor <= 0){
-            console.log('Valor indisponivel para deposito');
-        }else{
+        if(valor > 0){
             this.#saldo += valor;
+            console.log("Valor atual do Saldo após deposito: R$"+ this.#saldo);
+        }else{
+            console.log('Valor inválido para deposito');
         }
+    }
+
+    transferencia(valor,conta){
+
+        if(valor > 0){
+
+            if(valor > this.#saldo){
+                console.log('Saldo Insuficiente para Transferência, Saldo: R$' + this.#saldo);
+                return;
+            }
+            const valorSacado = this.sacar(valor);
+            conta.deposito(valorSacado);
+            console.log("Valor atual do Saldo após transferencia: R$"+ this.#saldo);
+
+        }else{
+            console.log('Valor inválido para transferencia');
+        }
+        
+
     }
 
 }
--- index.js
+++ index.js
@@ -2,27 +2,52 @@ import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 
 
-cliente1 = new Cliente();
+const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = "060.727.501-41";
 
-
-const contaCorrenteRicardo = new ContaCorrente();
-
-contaCorrenteRicardo.saldo = 0;
-contaCorrenteRicardo.agencia = 1001;
+const cliente2 = new Cliente();
+cliente2.nome = "Vinicius"
+cliente2.cpf = "688.038.663-15"
 
 
-const contaCorrenteAlice = new ContaCorrente();
+const contaCorrente1 = new ContaCorrente();
 
-contaCorrenteAlice.saldo = 10;
-contaCorrenteAlice.agencia = 1011;
+contaCorrente1.agencia = 1001;
+contaCorrente1.cliente = cliente1;
 
+const contaCorrente2 = new ContaCorrente();
 
-contaCorrenteAlice.deposito(10);
-contaCorrenteAlice.sacar(5);
-contaCorrenteAlice.sacar(6);
-contaCorrenteAlice.deposito(-1);
+contaCorrente2.agencia = 1002;
+contaCorrente2.cliente = cliente2;
+
+contaCorrente1.deposito(100);
+contaCorrente1.sacar(20);
+contaCorrente1.transferencia(70,contaCorrente2);
+
+console.log('------- Lançamento de exceções !! --------');
+
+console.log('------- Deposito --------');
+contaCorrente1.deposito(-1);
+contaCorrente1.deposito(0);
+contaCorrente1.deposito('a');
+
+console.log('------- Sacar --------');
+contaCorrente1.sacar(-1);
+contaCorrente1.sacar(0);
+contaCorrente1.sacar('a');
+contaCorrente1.sacar(90);
+
+console.log('------- Transferencia --------');
+contaCorrente1.transferencia(90,contaCorrente1);
+contaCorrente1.transferencia(0,contaCorrente1);
+contaCorrente1.transferencia(-1,contaCorrente1);
+contaCorrente1.transferencia('a',contaCorrente1);
+
+console.log('------------------------------------------');
+
 
 
 console.log(cliente1);
+
+console.log(cliente2);
